b6ef444d88c6b68e60a12b3c4bf5573c6abae7a3
FaRusDev <22978769+FaRusDev@users.noreply.github.com>
2025-04-01 22:06:23 +0700

    build: finish v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        width: "width",
+        height: "height",
+        spacing: "margin, padding",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          "0%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.3s ease-out",
+        "fade-out-up": "fade-out-up 0.3s ease-out",
+      },
+    },
   },
-  plugins: [],
 }
